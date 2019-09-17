1c5ecc1ff0d9744e7d9b92e7a58647ff31c6fd80
zhongzc <zhongzc_arch@outlook.com>
2019-09-17 20:49:51 +0800

    ast: clean up defination

--- main.go
+++ main.go
@@ -1,15 +1,4 @@
 package main
 
-import (
-	"fmt"
-	. "lexerGenerator/rgex"
-)
-
 func main() {
-	l := Literal{Rune: '好'}
-	c := Concat{Grounds: []Ground{l,
-		CharacterSet{Ranges: []Range{{From: 'A', To: 'Z'}}},
-		Repeat{Choose: Choose{Concats: []Concat{{Grounds: []Ground{l, CharacterSet{Ranges: []Range{{From: 'a', To: 'z'}}}}}}}}}}
-	ch := Choose{Concats: []Concat{c, c}}
-	fmt.Println(ch.ToRegex())
 }
